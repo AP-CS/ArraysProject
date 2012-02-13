85fb119f03369f6e492626736ab78c337fdaed75
Paul Buonopane <techsoftadvanced@gmail.com>
2012-02-13 09:12:22 -0500

    Created base for three applications.

---
+++ ArrayProjectA/src/com/earth2me/school/arraysproject/a/Main.java
@@ -0,0 +1,32 @@
+package com.earth2me.school.arraysproject.a;
+
+/**
+ * Contains the main entry point for the application
+ * 
+ * @author Paul Buonopane
+ * @category APCS
+ * @school Medfield High School
+ * @version 1.0.0
+ */
+public final class Main
+{
+	/**
+	 * Prevents static class from being instantiated. Do not use. Do not remove.
+	 * 
+	 * @deprecated
+	 */
+	private Main()
+	{
+		// Intentionally blank.
+	}
+
+	/**
+	 * Main entry point.
+	 * 
+	 * @param args
+	 *            Command-line arguments.
+	 */
+	public static void main(String[] args)
+	{
+	}
+}
