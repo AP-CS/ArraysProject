a3cbddc6417d2a70b32ca77c958b2ce4a4fc48c5
Paul Buonopane <techsoftadvanced@gmail.com>
2012-02-17 04:03:48 -0500

    Major refactoring.  Using Ant now.

---
+++ Eclipse/ArraysProjectLib/src/com/earth2me/school/arraysproject/Application.java
@@ -0,0 +1,51 @@
+package com.earth2me.school.arraysproject;
+
+import java.io.IOException;
+
+/**
+ * Executes programs as primary applications.  Cannot be instantiated.
+ * @author Paul Buonopane
+ * @category APCS
+ * @school Medfield High School
+ * @version 1.0.0
+ */
+public final class Application
+{
+	/**
+	 * Prevents type from being instantiated. Do not use. Do not remove.
+	 * 
+	 * @deprecated
+	 */
+	private Application()
+	{
+		// Intentionally blank.
+	}
+	
+	public static void run(IProgram program)
+	{
+		try
+		{
+			program.initialize();
+			program.run();
+		}
+		catch (Throwable ex)
+		{
+			Console.writeError("Program encountered an error:");
+			ex.printStackTrace();
+		}
+		finally
+		{
+			program.dispose();
+			
+			try
+			{
+				Console.writeLine("Press [Enter] to exit.");
+				System.in.read();
+			}
+			catch (IOException ex)
+			{
+				return;
+			}
+		}
+	}
+}
